27724e326dd979f71fab49b41ac9e3dd4d3a002e
junho <wnsgh1357@gmail.com>
2022-10-13 23:35:29 +0900

    Initial commit

---
+++ src/test/java/com/example/SpringSecurityJWTExample/SpringSecurityJwtExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityJWTExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
