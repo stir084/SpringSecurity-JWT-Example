ec2e4236f17616c6ed12e300e41e04ce9807fc49
junho <wnsgh1357@gmail.com>
2022-10-18 11:50:50 +0900

    Security defrecated Class deleted

--- src/main/java/com/example/SpringSecurityJWTExample/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurityJWTExample/config/SecurityConfig.java
@@ -3,18 +3,28 @@ package com.example.SpringSecurityJWTExample.config;
 import com.example.SpringSecurityJWTExample.config.jwt.JwtAuthenticationFilter;
 import com.example.SpringSecurityJWTExample.config.jwt.JwtAuthorizationFilter;
 import com.example.SpringSecurityJWTExample.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity // 시큐리티 활성화 -> 기본 스프링 필터체인에 등록
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+	private final AuthenticationConfiguration authenticationConfiguration;
 
 	@Autowired
 	private UserRepository userRepository;
@@ -27,7 +37,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		return new BCryptPasswordEncoder();
 	}
 
-	@Override
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.csrf().disable()
+				.addFilter(corsConfig.corsFilter())
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.formLogin().disable()
+				.httpBasic().disable()
+
+				.addFilter(new JwtAuthenticationFilter(authenticationManager()))
+				.addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository))
+				.authorizeRequests()
+				.antMatchers("/api/v1/user/**")
+				.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+				.antMatchers("/api/v1/manager/**")
+				.access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+				.antMatchers("/api/v1/admin/**")
+				.access("hasRole('ROLE_ADMIN')")
+				.anyRequest().permitAll()
+				.and().build();
+	}
+
+	/*@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 				.addFilter(corsConfig.corsFilter())
@@ -46,7 +83,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 				.antMatchers("/api/v1/admin/**")
 				.access("hasRole('ROLE_ADMIN')")
 				.anyRequest().permitAll();
-	}
+	}*/
 }
 
 
